15bc96e7d7a528728836d049144ea6d35d53212f
Richard Lincoln <r.w.lincoln@gmail.com>
2015-09-02 10:44:15 +0200

    Adding HTML file for key example.

--- dart/example/key.dart
+++ dart/example/key.dart
@@ -8,11 +8,11 @@ main() {
     {'id': '2', 'text': 'Two'},
     {'id': '3', 'text': 'Three'},
     {'id': '1', 'text': 'One'}
-  ].forEach((doc) => index.addDocument(doc));
+  ].forEach((doc) => index.add_document(doc));
 
   print(index.size); // => 3
-  print(index['1'].load.inspect); // => {'text': "One", 'id': "1"}
-  print(index.search('id:1').toString('text'));
+  print(index['1'].load()); // => {'text': "One", 'id': "1"}
+  print(index.search('id:1').to_s('text'));
   // => TopDocs: total_hits = 1, max_score = 1.287682 [
   //            3 "One": 1.287682
   //    ]
