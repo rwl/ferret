73ceff059ea4f4c7fe4df262df239266769ad8a0
Richard Lincoln <r.w.lincoln@gmail.com>
2015-09-06 00:17:28 +0200

    Documenting field info enums.

--- dart/lib/src/ext/index/field_info.dart
+++ dart/lib/src/ext/index/field_info.dart
@@ -1,44 +1,80 @@
 part of ferret.ext.index;
 
-class StoreValue {
-  final String _name;
-  final int value;
-  const StoreValue._internal(this._name, this.value);
-  toString() => 'StoreValue.$_name';
+/// Used to specify how fields are stored.
+class FieldStorage {
+  /// Don't store field.
+  static const NO = const FieldStorage._('NO', 0);
 
-  static const NO = const StoreValue._internal('NO', 0);
-  static const YES = const StoreValue._internal('YES', 1);
-  static const COMPRESS = const StoreValue._internal('COMPRESS', 2);
+  /// Store field in its original format. Use this value if you want to
+  /// highlight matches or print match excerpts a la Google search.
+  static const YES = const FieldStorage._('YES', 1);
+
+  /// Store field in compressed format.
+  static const COMPRESS = const FieldStorage._('COMPRESS', 2);
+
+  final String _name;
+  final int _value;
+
+  const FieldStorage._(this._name, this._value);
+
+  String toString() => 'FieldStorage.$_name';
 }
 
-class IndexValue {
-  final String _name;
-  final int value;
-  const IndexValue._internal(this._name, this.value);
-  toString() => 'IndexValue.$_name';
+/// Used to specify how fields are indexed.
+class FieldIndexing {
+  /// Do not make this field searchable.
+  static const NO = const FieldIndexing._('NO', 0);
 
-  static const NO = const IndexValue._internal('NO', 0);
-  static const UNTOKENIZED = const IndexValue._internal('UNTOKENIZED', 1);
-  static const YES = const IndexValue._internal('YES', 3);
+  /// Make this field searchable but do not tokenize its contents.
+  /// Use this value for fields you wish to sort by.
+  static const UNTOKENIZED = const FieldIndexing._('UNTOKENIZED', 1);
+
+  /// Make this field searchable and tokenized its contents.
+  static const YES = const FieldIndexing._('YES', 3);
+
+  /// Same as [UNTOKENIZED] except omit the norms file. Norms files can
+  /// be omitted if you don't boost any fields and you don't need
+  /// scoring based on field length.
   static const UNTOKENIZED_OMIT_NORMS =
-      const IndexValue._internal('UNTOKENIZED_OMIT_NORMS', 5);
-  static const YES_OMIT_NORMS = const IndexValue._internal('YES_OMIT_NORMS', 7);
+      const FieldIndexing._('UNTOKENIZED_OMIT_NORMS', 5);
+
+  /// Same as [YES] except omit the norms file. The norms file can
+  /// be omitted if you don't boost any fields and you don't need
+  /// scoring based on field length.
+  static const YES_OMIT_NORMS = const FieldIndexing._('YES_OMIT_NORMS', 7);
+
+  final String _name;
+  final int _value;
+
+  const FieldIndexing._(this._name, this._value);
+
+  String toString() => 'FieldIndexing.$_name';
 }
 
-class TermVectorValue {
-  final String _name;
-  final int value;
-  const TermVectorValue._internal(this._name, this.value);
-  toString() => 'TermVectorValue.$_name';
+/// Used to specify how term vectors are stored.
+class TermVectorStorage {
+  /// Don't store term-vectors.
+  static const NO = const TermVectorStorage._('NO', 0);
 
-  static const NO = const TermVectorValue._internal('NO', 0);
-  static const YES = const TermVectorValue._internal('YES', 1);
-  static const WITH_POSITIONS =
-      const TermVectorValue._internal('WITH_POSITIONS', 3);
-  static const WITH_OFFSETS =
-      const TermVectorValue._internal('WITH_OFFSETS', 5);
+  /// Store term-vectors without storing positions or offsets.
+  static const YES = const TermVectorStorage._('YES', 1);
+
+  /// Store term-vectors with positions.
+  static const WITH_POSITIONS = const TermVectorStorage._('WITH_POSITIONS', 3);
+
+  /// Store term-vectors with offsets.
+  static const WITH_OFFSETS = const TermVectorStorage._('WITH_OFFSETS', 5);
+
+  /// Store term-vectors with positions and offsets.
   static const WITH_POSITIONS_OFFSETS =
-      const TermVectorValue._internal('WITH_POSITIONS_OFFSETS', 7);
+      const TermVectorStorage._('WITH_POSITIONS_OFFSETS', 7);
+
+  final String _name;
+  final int _value;
+
+  const TermVectorStorage._(this._name, this._value);
+
+  String toString() => 'TermVectorStorage.$_name';
 }
 
 /// The [FieldInfos] class holds all the field descriptors for an index. It is
@@ -72,20 +108,20 @@ class TermVectorValue {
 /// along. If you add a document to the index which has fields that the index
 /// doesn't know about then the default properties are used for the new field.
 class FieldInfos extends JsProxy {
-  final StoreValue store;
-  final IndexValue index;
-  final TermVectorValue term_vector;
+  final FieldStorage store;
+  final FieldIndexing index;
+  final TermVectorStorage term_vector;
 
   /// Create a new [FieldInfos] object which uses the default values for
   /// fields specified in the [default_values] hash parameter. See [FieldInfo]
   /// for available property values.
   FieldInfos(
-      {this.store: StoreValue.YES,
-      this.index: IndexValue.YES,
-      this.term_vector: TermVectorValue.WITH_POSITIONS_OFFSETS})
+      {this.store: FieldStorage.YES,
+      this.index: FieldIndexing.YES,
+      this.term_vector: TermVectorStorage.WITH_POSITIONS_OFFSETS})
       : super() {
     handle = module.callMethod(
-        '_frt_fis_new', [store.value, index.value, term_vector.value]);
+        '_frt_fis_new', [store._value, index._value, term_vector._value]);
   }
 
   /// Return an array of the [FieldInfo] objects contained but this
@@ -137,9 +173,9 @@ class FieldInfos extends JsProxy {
   /// description of the available properties. Property values default to
   /// those used in the constructor.
   void add_field(String field,
-      {StoreValue store,
-      IndexValue index,
-      TermVectorValue term_vector,
+      {FieldStorage store,
+      FieldIndexing index,
+      TermVectorStorage term_vector,
       double boost: 1.0}) {
     if (store == null) {
       store = this.store;
@@ -151,8 +187,14 @@ class FieldInfos extends JsProxy {
       term_vector = this.term_vector;
     }
     int p_field = allocString(field);
-    module.callMethod('_frjs_fis_add_field',
-        [handle, p_field, store.value, index.value, term_vector.value, boost]);
+    module.callMethod('_frjs_fis_add_field', [
+      handle,
+      p_field,
+      store._value,
+      index._value,
+      term_vector._value,
+      boost
+    ]);
     free(p_field);
   }
 
@@ -229,121 +271,56 @@ class FieldInfos extends JsProxy {
 /// can also set the default [boost] for a field as well.
 ///
 /// The [store] property allows you to specify how a field is stored. You can
-/// leave a field unstored (`no`), store it in it's original format (`yes`)
-/// or store it in compressed format (`compressed`). By default the document
-/// is stored in its original format. If the field is large and it is stored
-/// elsewhere where it is easily accessible you might want to leave it
-/// unstored. This will keep the index size a lot smaller and make the
-/// indexing process a lot faster. For example, you should probably leave the
-/// [content] field unstored when indexing all the documents in your
-/// file-system.
+/// leave a field unstored ([FieldStorage.NO]), store it in it's original
+/// format ([FieldStorage.YES]) or store it in compressed format
+/// ([FieldStorage.COMPRESSED]). By default the document is stored in its
+/// original format. If the field is large and it is stored elsewhere where it
+/// is easily accessible you might want to leave it unstored. This will keep
+/// the index size a lot smaller and make the indexing process a lot faster.
+/// For example, you should probably leave the `content` field unstored when
+/// indexing all the documents in your file-system.
 ///
 /// The [index] property allows you to specify how a field is indexed. A
 /// field must be indexed to be searchable. However, a field doesn't need to
 /// be indexed to be store in the Ferret index. You may want to use the index
 /// as a simple database and store things like images or MP3s in the index. By
-/// default each field is indexed and tokenized (split into tokens) (`yes`).
-/// If you don't want to index the field use `no`. If you want the field
-/// indexed but not tokenized, use `untokenized`. Do this for the fields you
-/// wish to sort by. There are two other values for [index]; `omit_norms`
-/// and `untokenized_omit_norms`. These values correspond to `yes` and
-/// `untokenized` respectively and are useful if you are not boosting any
-/// fields and you'd like to speed up the index. The norms file is the file
-/// which contains the boost values for each document for a particular field.
+/// default each field is indexed and tokenized (split into tokens)
+/// ([FieldIndexing.YES]). If you don't want to index the field use
+/// [FieldIndexing.NO]. If you want the field indexed but not tokenized, use
+/// [FieldIndexing.UNTOKENIZED]. Do this for the fields you wish to sort by.
+/// There are two other values for [index]; [FieldIndexing.YES_OMIT_NORMS]
+/// and [FieldIndexing.UNTOKENIZED_OMIT_NORMS]. These values correspond to
+/// [FieldIndexing.YES] and [FieldIndexing.UNTOKENIZED] respectively and are
+/// useful if you are not boosting any fields and you'd like to speed up the
+/// index. The norms file is the file which contains the boost values for each
+/// document for a particular field.
 ///
 /// See [TermVector] for a description of term-vectors. You can specify
-/// whether or not you would like to store term-vectors. The available options
-/// are `no`, `yes`, `with_positions`, `with_offsets` and
-/// `with_positions_offsets`. Note that you need to store the positions to
-/// associate offsets with individual terms in the term_vector.
+/// whether or not you would like to store term-vectors. Note that you need
+/// to store the positions to associate offsets with individual terms in the
+/// term_vector.
 ///
-///     Property       Value                     Description
-///     ------------------------------------------------------------------------
-///     :store       | :no                     | Don't store field
-///                  |                         |
-///                  | :yes (default)          | Store field in its original
-///                  |                         | format. Use this value if you
-///                  |                         | want to highlight matches.
-///                  |                         | or print match excerpts a la
-///                  |                         | Google search.
-///                  |                         |
-///                  | :compressed             | Store field in compressed
-///                  |                         | format.
-///     -------------|-------------------------|------------------------------
-///     :index       | :no                     | Do not make this field
-///                  |                         | searchable.
-///                  |                         |
-///                  | :yes (default)          | Make this field searchable and
-///                  |                         | tokenized its contents.
-///                  |                         |
-///                  | :untokenized            | Make this field searchable but
-///                  |                         | do not tokenize its contents.
-///                  |                         | use this value for fields you
-///                  |                         | wish to sort by.
-///                  |                         |
-///                  | :omit_norms             | Same as :yes except omit the
-///                  |                         | norms file. The norms file can
-///                  |                         | be omitted if you don't boost
-///                  |                         | any fields and you don't need
-///                  |                         | scoring based on field length.
-///                  |                         |
-///                  | :untokenized_omit_norms | Same as :untokenized except omit
-///                  |                         | the norms file. Norms files can
-///                  |                         | be omitted if you don't boost
-///                  |                         | any fields and you don't need
-///                  |                         | scoring based on field length.
-///                  |                         |
-///     -------------|-------------------------|------------------------------
-///     :term_vector | :no                     | Don't store term-vectors
-///                  |                         |
-///                  | :yes                    | Store term-vectors without
-///                  |                         | storing positions or offsets.
-///                  |                         |
-///                  | :with_positions         | Store term-vectors with
-///                  |                         | positions.
-///                  |                         |
-///                  | :with_offsets           | Store term-vectors with
-///                  |                         | offsets.
-///                  |                         |
-///                  | :with_positions_offsets | Store term-vectors with
-///                  | (default)               | positions and offsets.
-///     -------------|-------------------------|------------------------------
-///     :boost       | Float                   | The boost property is used to
-///                  |                         | set the default boost for a
-///                  |                         | field. This boost value will
-///                  |                         | used for all instances of the
-///                  |                         | field in the index unless
-///                  |                         | otherwise specified when you
-///                  |                         | create the field. All values
-///                  |                         | should be positive.
-///                  |                         |
+/// The [boost] property is used to set the default boost for a field. This
+/// boost value will used for all instances of the field in the index unless
+/// otherwise specified when you create the field. All values should be
+/// positive.
 ///
-///     var fi = new FieldInfo('title', index: 'untokenized', term_vector: 'no',
+///     var fi = new FieldInfo('title',
+///       index: FieldIndexing.UNTOKENIZED,
+///       term_vector: TermVectorStorage.NO,
 ///       boost: 10.0);
 ///
 ///     var fi = new FieldInfo('content');
 ///
-///     var fi = new FieldInfo('created_on', index: 'untokenized_omit_norms',
-///       term_vector: 'no');
+///     var fi = new FieldInfo('created_on',
+///       index: FieldIndexing.UNTOKENIZED_OMIT_NORMS,
+///       term_vector: TermVectorStorage.NO);
 ///
-///     var fi = new FieldInfo('image', store: 'compressed', index: 'no',
-///       term_vector: 'no');
+///     var fi = new FieldInfo('image',
+///       store: FieldStorage.COMPRESSED,
+///       index: FieldIndexing.NO,
+///       term_vector: TermVectorStorage.NO);
 class FieldInfo extends JsProxy {
-  var _store;
-  var _index;
-  var _term_vector;
-
-  var _compress;
-  var _compressed;
-
-  var _untokenized;
-  var _omit_norms;
-  var _untokenized_omit_norms;
-
-  var _with_positions;
-  var _with_offsets;
-  var _with_positions_offsets;
-
   FieldInfo._handle(int hfi) : super() {
     handle = hfi;
   }
@@ -352,7 +329,10 @@ class FieldInfo extends JsProxy {
   /// specified in [options]. The available options are [`store`, `index`,
   /// `term_vector`, `boost`]. See the description of [FieldInfo] for more
   /// information on these properties.
-  FieldInfo(name, options) {
+  FieldInfo(name,
+      {store: FieldStorage.YES,
+      index: FieldIndexing.YES,
+      term_vector: TermVectorStorage.WITH_POSITIONS_OFFSETS}) {
     frb_fi_init;
   }
 
